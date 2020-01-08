9a87441690767659d4f18c825d845d623cf87e7b
HarworkingSnowman <cpr1014@gmail.com>
2020-01-08 18:37:55 +0800

    HCSR04 start

--- Project/AdvanceAlarm/src/main.c
+++ Project/AdvanceAlarm/src/main.c
@@ -1,5 +1,16 @@
-#include "bsp_sw420.h"
+#include "stm32l476xx.h"
+#include "utility.h"
+//#include <stdio.h>
 
-int main(){
+extern void gpio_init();
+
+int Distance;
+
+int main(void) {
+	gpio_init();
+	counter_init();
+	//printf("%d", HCSR04GetDistance());
 
 }
+
+
